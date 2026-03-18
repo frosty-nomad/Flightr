a182959757ccfde1d2e1b0914f64c076144498dd
FrostyNomad <37113023+frosty-nomad@users.noreply.github.com>
2026-03-18 21:10:45 +0000

    Handle account creation and login

--- Flightr.Api/Program.cs
+++ Flightr.Api/Program.cs
@@ -32,8 +32,6 @@ var connectionString = builder.Configuration.GetConnectionString("DefaultConnect
 builder.Services.AddDbContext<FlightrDbContext>(options =>
     options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString)));
 
-builder.Services.AddAuthentication()
-    .AddBearerToken(IdentityConstants.BearerScheme);
 builder.Services.AddAuthorization();
 
 builder.Services.AddIdentityApiEndpoints<ApplicationUser>()
